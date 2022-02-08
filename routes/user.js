9338d9d706ac59e8867037c432f4e0518114ef95
Aaron <aarongraybill3@gmail.com>
2022-02-08 01:44:02 -0600

    init create user

--- routes/user.js
+++ routes/user.js
@@ -14,5 +14,55 @@ router.post('/', [
     check('email', 'A Valid Email is Required').isEmail(),
     check('password', 'Please Enter A Password With 6 Or More Characters').isLength({min: 6})
 ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-})
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({errors: [{msg: 'User Already Exists'}]});
+        }
+        else {
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({
+                    name,
+                    email, 
+                    password, 
+                    avatar
+            });
+
+            const salt = await bc.genSalt(10);
+
+            user.password = await bc.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 3600000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+
+            console.log(req.body);
+        }
+    } catch (err) {
+        
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const connectDB = require('./config/db');
 connectDB();
 
 app.use(express.json({ extended: false }));
-app.use(cors);
+app.use(cors());
 
 app.get('/', (req, res) => res.send('API Running'));
 
-app.use('/api/users', require('./api/users'));
-app.use('/api/recipes', require('./api/recipes'));
-app.use('/api/ingredients', require('./api/ingredients'));
+app.use('/api/user', require('./routes/user'));
+// app.use('/api/recipes', require('./routes/recipes'));
+// app.use('/api/ingredients', require('./routes/ingredients'));
 
 const PORT = process.env.PORT || 5000;
 
