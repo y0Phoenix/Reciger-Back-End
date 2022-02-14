1475ab6310facc2c36f32f28b3db0463686f96be
Aaron <aarongraybill3@gmail.com>
2022-02-14 12:04:17 -0600

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { check, validationResult } = require('express-validator');
 const bc = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
